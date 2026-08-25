d4274a19f935b1653b7de0181d0f5a54634af069
AyushGiri2508 <ayushgiri9988777@gmail.com>
2026-08-25 09:55:38 +0530

    Added:Render Hitting

--- server/server.js
+++ server/server.js
@@ -4,6 +4,30 @@ const app = require("./src/app");
 const connectDB = require("./src/config/database");
 const { connectCloudinary } = require("./src/config/cloudinary");
 const dns = require("dns");
+const axios = require("axios");
+
+const url = process.env.SERVER_URL;
+const interval = 30000;
+
+function reloadWebsite() {
+  axios
+    .get(url)
+    .then((response) => {
+      console.log("Website reloaded");
+    })
+    .catch((error) => {
+      console.error(`Error: ${error.message}`);
+    });
+}
+
+setInterval(reloadWebsite, interval);
+
+
+
+
+
+
+
 
 // Change DNS servers
 dns.setServers(["1.1.1.1", "8.8.8.8"]);
